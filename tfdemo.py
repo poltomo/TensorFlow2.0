625afc2e813fae31e63c396337850420555cd5de
Varun Nawathey <vnaw4988@gmail.com>
2022-05-05 20:20:06 -0700

    added tf normal distribution demo

--- tfdemo.py
+++ tfdemo.py
@@ -1,9 +1,12 @@
 import tensorflow as tf
 
 # vector is a one dimensional tensor
-# matrix is a two dimensional tensor
 x=tf.constant(5)
 print(x)
 
+# matrix is a two dimensional tensor
 x=tf.constant([[1,2,3],[4,5,6]])
+print(x)
+
+x=tf.random.normal((3,3), mean=0, stddev=1)
 print(x)
