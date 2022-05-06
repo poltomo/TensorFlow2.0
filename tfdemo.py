2a8749d0beb0d892674298d9b754e5975250bc47
Varun Nawathey <vnaw4988@gmail.com>
2022-05-05 20:26:43 -0700

    demo of tensor indexing and reshaping

--- tfdemo.py
+++ tfdemo.py
@@ -8,5 +8,22 @@ print(x)
 x=tf.constant([[1,2,3],[4,5,6]])
 print(x)
 
+# tensorflow normal distribution
 x=tf.random.normal((3,3), mean=0, stddev=1)
 print(x)
+
+# vector       0,1,2,3,4,5,6
+x=tf.constant([1,2,3,4,5,9,8])
+
+# tensor indexing
+print(x)
+print(x[::])
+print(x[4:])
+print(x[::-1])
+print(x[::3])
+
+x=tf.range(9)
+print(x)
+
+#vector -> matrix 3*3
+y=tf.reshape(x,(3,3))
