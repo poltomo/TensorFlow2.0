098b6fb2903f340f2344e758e4f7689c436f2bae
Varun Nawathey <vnaw4988@gmail.com>
2022-05-07 16:48:43 -0700

    created digit classification model

---
+++ digitRecognition.py
@@ -0,0 +1,44 @@
+from this import d
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras.layers import Flatten, Dense
+from tensorflow.keras import Sequential
+
+mnist = keras.datasets.mnist
+
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+#print(x_train.shape)
+#print(x_test.shape)
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+#print(np.min(x_train))
+#print(np.max(x_train))
+
+plt.figure()
+plt.imshow(x_train[0])
+plt.colorbar()
+
+x_train = x_train/255.0
+x_test = x_test/255.0
+
+model = Sequential()
+model.add(Flatten(input_shape=(28,28)))
+model.add(Dense(128, activation="relu"))
+model.add(Dense(10, activation="softmax"))
+
+print(model.summary())
+
+model.compile(
+    optimizer="adam",
+    loss="sparse_categorical_crossentropy",
+    metrics=["accuracy"]
+)
+
+model.fit(x_train, y_train, epochs=10)
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(test_loss)
+print(test_acc)
